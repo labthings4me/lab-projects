a83e4900ef8a9067a160532ff77666b49b05ac27
zoe <zoehedstrom@yahoo.com>
2021-11-16 14:07:23 -0500

    main

---
+++ backgroundcolor/emptyexample/sketch.js
@@ -0,0 +1,8 @@
+function setup() {
+  createCanvas (windowWidth,windowHeight);//this creates window width and height to be sixe of browser
+  background (255,0,0); // red bcakground
+}
+
+function draw() {
+  ;
+}
