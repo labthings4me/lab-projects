e85d6008748cfa5032ae81bc8d3b24cc35e55e0a
labthings4me <zoehedstrom@yahoo.com>
2021-12-01 19:45:57 -0500

    try

---
+++ CreativeCoding/P5Js/twoxtwo/sketch.js
@@ -0,0 +1,23 @@
+let img;
+
+function preload () {
+  img = loadImage ('assets/lizardnew.png');
+}
+
+function setup() {
+  createCanvas(720, 720);
+  fill(207, 238, 250);
+  strokeWeight(5);
+}
+
+function draw() {
+
+  image (img, 0,0);
+  // background(220);
+  // translate(60, 60);
+  // quad(0, 0, 300, 0, 300, 300, 0, 300);
+  // quad(0 + 300, 0, 600, 0, 600, 300, 300, 300); // increase x 300px
+  //  quad(0 + 300, 300, 600, 300, 600, 600, 300, 600); // increase y 300px
+  //   quad(0, 300, 300, 300, 300, 600, 0, 600); //increase y 300px
+  // noLoop();
+}
