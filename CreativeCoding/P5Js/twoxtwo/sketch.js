acd9763d1c5a0c3aab1a82951d60da5ffcddded6
labthings4me <zoehedstrom@yahoo.com>
2021-12-01 21:25:54 -0500

    Update sketch.js

--- CreativeCoding/P5Js/twoxtwo/sketch.js
+++ CreativeCoding/P5Js/twoxtwo/sketch.js
@@ -13,11 +13,11 @@ function setup() {
 function draw() {
 
   image (img, 0,0);
-  // background(220);
-  // translate(60, 60);
-  // quad(0, 0, 300, 0, 300, 300, 0, 300);
-  // quad(0 + 300, 0, 600, 0, 600, 300, 300, 300); // increase x 300px
-  //  quad(0 + 300, 300, 600, 300, 600, 600, 300, 600); // increase y 300px
-  //   quad(0, 300, 300, 300, 300, 600, 0, 600); //increase y 300px
-  // noLoop();
+  background(220);
+  translate(60, 60);
+  quad(0, 0, 300, 0, 300, 300, 0, 300);
+  quad(0 + 300, 0, 600, 0, 600, 300, 300, 300); // increase x 300px
+   quad(0 + 300, 300, 600, 300, 600, 600, 300, 600); // increase y 300px
+    quad(0, 300, 300, 300, 300, 600, 0, 600); //increase y 300px
+  noLoop();
 }
