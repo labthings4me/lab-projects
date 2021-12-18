08b9a52c70f0c099eb59bfdc213bad40271e7a0e
labthings4me <zoehedstrom@yahoo.com>
2021-12-18 10:26:04 -0500

    push

--- CreativeCoding/me/sketch.js
+++ CreativeCoding/me/sketch.js
@@ -1,23 +1,22 @@
 
-
 let gif;
 
 function preload (){
-  gif = loadImage ('Lizard.gif');
+  gif = loadImage ('cryptoselfiecode1.gif');
 }
 
 function setup() { //runs once
   createCanvas(windowWidth, windowHeight);
-  fill(206,250.5);
-  strokeWeight(1);
+  fill(65,101.176);
+  strokeWeight(0);
 }
 
 function draw() {
-background (206,250,5);
+background (65,101.176);
 
 translate(-100, -100)
 
-var num = 4; //variable for the number of squares in the array
+var num = 6; //variable for the number of squares in the array
 var sideLen = windowWidth/num;//variable for the side lenth of each square
 
   for (var y = 0; y < 2*windowHeight; y = y + sideLen) {//loop to create rows in the y direction
